c9f81f26fd11f3d3e0f2d871aec195fbac178e0b
bmaity <35411412+bmaity@users.noreply.github.com>
2019-06-25 18:58:29 -0700

    Update avg.py

--- avg.py
+++ avg.py
@@ -8,3 +8,16 @@ for value in [3,41,75,40,12,55,65]:
     sum = sum + value
     print(count, sum, value)
 print('After: Count, sum, average ', count, sum, sum/count)
+
+
+'''Another way of doing it '''
+
+'''Finding avg'''
+#count = 0
+#sum = 0
+#print ('Before count and sum value:', count, sum)
+#for value in [3,5,18,65,74,78]:
+#   count = count + 1
+#   sum = sum + value 
+#   print('Current value of count, sum, value', count, sum, value)
+#print('After the loop iteration the avg value is', sum/count)
