557b54892837c81495b1e979555489dc510faed7
bmaity <35411412+bmaity@users.noreply.github.com>
2019-06-25 18:42:42 -0700

    Create largest_in_loop.py

---
+++ largest_in_loop.py
@@ -0,0 +1,8 @@
+'''largest in a loop'''
+largest_so_far = -1
+print('Before', largest_so_far)
+for the_num in [9,41,3,12,74,5]:
+    if the_num > largest_so_far:
+        largest_so_far = the_num
+        print('largest_so_far', the_num)
+print('After', largest_so_far)
