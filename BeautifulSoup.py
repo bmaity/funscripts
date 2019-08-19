829c337f9c1c1278460197029a80a5281aff7da8
bmaity <35411412+bmaity@users.noreply.github.com>
2019-08-18 17:51:03 -0700

    Update BeautifulSoup.py

--- BeautifulSoup.py
+++ BeautifulSoup.py
@@ -11,3 +11,26 @@ soup = BeautifulSoup(html,'html.parser')
 tags = soup('a')
 for tag in tags:
     print(tag.get('href', None))
+    
+    
+    
+#newer version with SSL check
+import urllib.request, urllib.parse, urllib.error
+from bs4 import BeautifulSoup
+import ssl
+
+#Ignore SSL certiticate errors 
+ctx = ssl.create_default_context()
+ctx.check_hostname = False
+ctx.verify_mode = ssl.CERT_NONE
+
+#Reading the URL
+url = input('Enter- ')
+#html = urllib.request.urlopen(url).read()
+html = urllib.request.urlopen(url, context=ctx).read()
+soup = BeautifulSoup(html,'html.parser')
+
+#Retrieve all anchor tags
+tags = soup('a')
+for tag in tags:
+    print(tag.get('href',None))
