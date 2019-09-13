a11133869596a4379764a8bdcb74c2a472cce2b9
bmaity <35411412+bmaity@users.noreply.github.com>
2019-09-13 02:39:33 -0700

    Update extract_data_from_JSON.py

--- extract_data_from_JSON.py
+++ extract_data_from_JSON.py
@@ -15,13 +15,13 @@ while True:
         if len(url)<1: break
         
         print('Retrieving', url)
-        uh = urllib.request.urlopen(url, context=ctx)
-        data = uh.read()
+        uh = urllib.request.urlopen(url, context=ctx). #Opening the URL
+        data = uh.read() #Raw Data 
 
         total = 0
         print('Retrieved', len(data),'characters')
 
-        info = json.loads(data)
+        info = json.loads(data) #Parsed data
         info = info["comments"]
         for item in info:
             print("Count: ",item["count"])
@@ -29,3 +29,18 @@ while True:
             print("Sum: ", total)
         print("Final sum: ", total)
         break
+
+   #Another way
+#url = raw_input("Enter json URL: ")
+#connection = urllib2.urlopen(url)
+#raw_data = connection.read()
+#parsed_data = json.loads(raw_data)
+#counts = []
+
+#comments = parsed_data["comments"]
+
+#for comment in comments:
+#    counts.append(comment['count'])
+
+#print "Count: {0}".format(len(counts))
+#print "Sum: {0}".format(sum(counts))
