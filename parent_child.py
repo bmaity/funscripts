c0aef79b02dabb4e1d28ef1e3560f9e2f6c29db3
Biswajit <biswajit.maity1509@gmail.com>
2021-04-09 01:08:39 -0700

    adding parent child process relation

---
+++ parent_child.py
@@ -0,0 +1,18 @@
+import os
+import sys
+while 1:
+    i = input("$")
+    params = i.split()
+    if params[0] == "cd":
+	os.chdir(params[1])
+    else:
+	
+    newpid = os.fork()
+    if newpid == 0:
+        print("I am the child process")
+        #exit()
+        os.execvp(params[0],params)
+    else:
+        print("I am the parent process")
+        print("The child process id is",newpid)
+        os.wait()
